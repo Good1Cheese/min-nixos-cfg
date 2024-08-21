defbeaae5e842566af46878d73949cb02366c257
Good1Cheese <work.cheese@erine.email>
2024-08-21 10:17:44 +0900

    libreoffice

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -8,38 +8,37 @@
 
   environment.systemPackages = with pkgs; [
     papirus-nord
-    home-manager
 	libsForQt5.qtstyleplugin-kvantum
 	libsForQt5.qt5ct
 
 	# imp
-	pkgs.waybar wl-clipboard xwayland
+	pkgs.waybar home-manager
+	wl-clipboard xwayland
 	gammastep swaybg mako
-	foot fish
-	fuzzel bemenu
-	clipman
 
     # utils
 	espeak-classic
 	sqlite wget git stow gcc
-	gnumake devour fastfetch
+	gnumake fastfetch
 	zip unzip unar unrar-wrapper
 	lshw ffmpeg openssl pugixml
 	doas atool bat
 	trash-cli gtrash
-	bluez bluez-tools bluetuith
 
 	# workflow
 	yazi jq fd ouch ffmpegthumbnailer poppler broot fzf 
-	neovim vim ripgrep silicon
+	foot fish
+	russ goread nom
+	bemenu clipman
+	neovim ripgrep silicon
 	lazygit gdu btop
+	mpv nomacs
 	python312Packages.adblock qutebrowser
 
 	# programs
 	firefox burpsuite zap
 	pavucontrol heroic
 	telegram-desktop gajim
-
 	drawing libreoffice
   ];
 
